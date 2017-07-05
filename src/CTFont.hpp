91925378d03933c5da0e7bb4fbee2bca43e713ff
Steve Varga <steve@vargatron.com>
2017-07-04 22:10:14 -0400

    Updated a bit

--- src/CTFont.hpp
+++ src/CTFont.hpp
@@ -16,42 +16,45 @@
 #include "harfbuzz/hb.h"
 #include "harfbuzz/hb-ft.h"
 
+#include "cinder/Channel.h"
 #include "cinder/DataSource.h"
 
 using namespace cinder;
 
-namespace typography {
-    class Font;
-    typedef std::shared_ptr<Font> FontRef;
-    
-    class Font {
-    public:
-        static FontRef create(ci::DataSourceRef dataSource, float size);
-        ~Font();
-        
-        float getSize() { return mSize; };
-        FT_Face &getFace() { return mFace; };
-        hb_font_t *getHarfbuzzFont() { return mHbFont; }
-        
-        ci::ChannelRef getCharacterBitmapChannel(char character);
-        ci::ChannelRef getBitmapChannel(FT_UInt glyphIndex);
-        
-        FT_Glyph_Metrics getMetrics(FT_UInt glyphIndex);
-        
-        const std::vector<FT_ULong> &getGlyphIndices() { return mGlyphIndices; };
-        
-    private:
-        Font(ci::DataSourceRef dataSource, float size);
-        
-        bool loadGlyphAtIndex(FT_UInt index);
-        
-        float mSize;
-        FT_Face mFace;
-        hb_font_t *mHbFont;
-        
-        // Map of glyphs
-        std::vector<FT_ULong> mGlyphIndices;
-    };
-    
-    
+namespace typography
+{
+	class Font;
+	typedef std::shared_ptr<Font> FontRef;
+
+	class Font
+	{
+		public:
+			static FontRef create( ci::DataSourceRef dataSource, float size );
+			~Font();
+
+			float getSize() { return mSize; };
+			FT_Face& getFace() { return mFace; };
+			hb_font_t* getHarfbuzzFont() { return mHbFont; }
+
+			ci::ChannelRef getCharacterBitmapChannel( char character );
+			ci::ChannelRef getBitmapChannel( FT_UInt glyphIndex );
+
+			FT_Glyph_Metrics getMetrics( FT_UInt glyphIndex );
+
+			const std::vector<FT_ULong>& getGlyphIndices() { return mGlyphIndices; };
+
+		private:
+			Font( ci::DataSourceRef dataSource, float size );
+
+			bool loadGlyphAtIndex( FT_UInt index );
+
+			float mSize;
+			FT_Face mFace;
+			hb_font_t* mHbFont;
+
+			// Map of glyphs
+			std::vector<FT_ULong> mGlyphIndices;
+	};
+
+
 }
--- src/CTRendererTexture.hpp
+++ src/CTRendererTexture.hpp
@@ -8,25 +8,29 @@
 
 #pragma once
 
+#include "cinder/gl/gl.h"
 #include "CTRenderer.hpp"
+#include <map>
 
-namespace typography {
-    typedef std::shared_ptr<class RendererTexture> RendererTextureRef;
-    
-    class RendererTexture : public Renderer {
-    public:
-        using GlyphTextureMap = std::map<FontRef, std::map<unsigned long, ci::gl::TextureRef> >;
-        
-        static RendererTextureRef create();
-        
-        void draw() override;
-        void setLayout(LayoutRef mLayout) override;
-        
-    private:
-        RendererTexture() {};
-        
-        void generateTexturesForFont(FontRef font);
-        
-        static GlyphTextureMap glyphTextures;
-    };
+namespace typography
+{
+	typedef std::shared_ptr<class RendererTexture> RendererTextureRef;
+
+	class RendererTexture : public Renderer
+	{
+		public:
+			using GlyphTextureMap = std::map<FontRef, std::map<unsigned long, ci::gl::TextureRef> >;
+
+			static RendererTextureRef create();
+
+			void draw() override;
+			void setLayout( LayoutRef mLayout ) override;
+
+		private:
+			RendererTexture() {};
+
+			void generateTexturesForFont( FontRef font );
+
+			static GlyphTextureMap glyphTextures;
+	};
 }
